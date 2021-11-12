b23b22ddf733d0fa279c597c23a7d6789b27dca2
Ohad Eytan <ohadey@gmail.com>
2021-11-12 13:06:46 +0200

    mouse: left handed configuration fix

--- .config/qtile/mouse.sh
+++ .config/qtile/mouse.sh
@@ -4,7 +4,7 @@
 xinput set-prop "TPPS/2 IBM TrackPoint" "libinput Accel Speed" 1
 # Switch left and right buttons
 xinput set-prop "TPPS/2 IBM TrackPoint" "libinput Left Handed Enabled" 1
-xinput set-prop "Logitech MX Anywhere 3" 336 1
+xinput set-prop "Logitech MX Anywhere 3" "libinput Left Handed Enabled" 1
 # Enable tapping
 xinput set-prop "Synaptics TM3276-022" "libinput Tapping Enabled" 1
 # Enable edge scrolling
